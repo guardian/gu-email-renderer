f848ab23834819366d45c7660ec94effc8cf7f4f
James Rodgers <james.rodgers@guardian.co.uk>
2012-12-13 17:11:01 +0000

    Classes are beginning to take shape.

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ jinja_environment = jinja2.Environment(
 
 class MainPage( webapp2.RequestHandler ):
     def get( self ):
-
         template = jinja_environment.get_template( 'index.html' )
-        template_values = {}
-        DataSource('').fetch_data()
-        self.response.out.write( template.render( template_values ) )
+
+        culture_source = DataSource('search', {'tag': 'type/article', 'section': 'culture', 'show-fields': 'all', 'lead-content': 'culture/culture'})
+
+        page_renderer = PageRenderer({'culture': culture_source}, 'index.html')
+        page = page_renderer.render()
+
+        self.response.out.write(page)
 
 
 app = webapp2.WSGIApplication( [ ( '/', MainPage ) ], debug=True )
@@ -25,31 +28,59 @@ app = webapp2.WSGIApplication( [ ( '/', MainPage ) ], debug=True )
 class PageRenderer:
 
     def __init__(self, data_source_map, template_name):
+        """
+        template_name is the name of a jinja_template.
+
+        data_source_map is a map whose values are objects which know how
+        to fetch json data from the api, and return that data as a
+        python map; the keys specify interpolation points in the
+        template.
+        """
         self.data_source_map = data_source_map
+        self.template = jinja_environment.get_template(template_name)
+
         self.template_name = template_name
 
-    def render (self):
-        # iterate through map. call fetch on each data_source.
-        # turn json into map
-        # put string and map onto view
-        pass
+    def render(self):
+        """
+        Fetch the json data over the wire and interpolate it into the
+        template specified by *template_name*
+        """
+        data = self._fetch_data()
+        return self.template.render(**data)
 
+    def _fetch_data(self):
+        """
+        Turn my data_source_map into a data map by calling fetch on each
+        value. The keys of the resulting map correspond exactly to those
+        of data_source_map.
+        """
+        fetched_data = {}
+
+        for data_source_name in self.data_source_map:
+            data_source = self.data_source_map[data_source_name]
+            data = data_source.fetch()
+            fetched_data[data_source_name] = data
+
+        return fetched_data
 
 class DataSource:
 
-    base_url = 'http://content.guardianapis.com/'
+    base_url = 'http://content.guardianapis.com/%s?%s'
     api_key = '***REMOVED***'
 
     #fields = ['trailText', 'headline', 'liveBloggingNow', 'standfirst', 'commentable']
 
 
-    def __init__(self, end_point, params):
-        params['api-key'] = api_key
-        self.api_url = base_url + endpoint + urllib.urlencode(params)
+    def __init__(self, endpoint, params):
+        params['api-key'] = self.api_key
+        params['format'] = 'json'
+        self.api_url = self.base_url  % (endpoint, urllib.urlencode(params))
 
-    def fetch_data(self):
+    def fetch(self):
         try:
-            response = urllib2.urlopen(api_url)
+            logging.info("Here is the api url: " + self.api_url)
+            response = urllib2.urlopen(self.api_url)
             response_body = response.read()
             json_response = json.loads(response_body)
 
@@ -57,7 +88,6 @@ class DataSource:
             status = response_object['status']
             results = response_object['results']
 
-            logging.info(fields)
             return results
 
         except urllib2.URLError, e:
