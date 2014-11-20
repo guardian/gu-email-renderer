42cf1b8e9bfef66ace1fe297d2a636a14f7e10cb
Robert Rees <robert.rees@guardian.co.uk>
2014-11-20 13:11:14 +0000

    What to wear has finished so remove the video from the email

--- email_definitions/fashion.py
+++ email_definitions/fashion.py
@@ -17,7 +17,6 @@ class FashionStatement(mr.EmailTemplate):
 
     base_data_sources = make_dict({
         'fashion_gallery': ds.FashionGalleryDataSource(client),
-        'fashion_video': ds.FashionVideoDataSource(client),
 		'fashion_most_viewed': ds.FashionMostViewedDataSource(client),
  
 	})
@@ -40,8 +39,8 @@ class FashionStatement(mr.EmailTemplate):
     data_sources['v2'] = data_sources['v1']
 
     priority_list = {
-        'v1': [('fashion_hadley', 1), ('fashion_video', 1), ('fashion_most_viewed', 6), ('fashion_news', 3), ('fashion_blog', 6), ('fashion_network', 6), ('fashion_gallery', 1)],
-        'v3': [('fashion_video', 1), ('fashion_hadley', 1), ('fashion_sali', 1), ('fashion_stylewatch', 1), ('fashion_picks', 5), ('fashion_most_viewed', 6), ('fashion_gallery', 1)]
+        'v1': [('fashion_hadley', 1), ('fashion_most_viewed', 6), ('fashion_news', 3), ('fashion_blog', 6), ('fashion_network', 6), ('fashion_gallery', 1)],
+        'v3': [('fashion_hadley', 1), ('fashion_sali', 1), ('fashion_stylewatch', 1), ('fashion_picks', 5), ('fashion_most_viewed', 6), ('fashion_gallery', 1)]
     }
     priority_list['v2'] = priority_list['v1']
 
