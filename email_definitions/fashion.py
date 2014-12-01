e1ab23c77da57a2203f1f26208b05d4331d29e90
Robert Rees <robert.rees@guardian.co.uk>
2014-12-01 14:33:13 +0000

    Restructures the template structure for Fashion Statement

--- email_definitions/fashion.py
+++ email_definitions/fashion.py
@@ -1,4 +1,4 @@
-from pysistence import make_dict
+import pysistence as immutable
 
 import mail_renderer as mr
 
@@ -16,10 +16,10 @@ class FashionStatement(mr.EmailTemplate):
         'leaderboard_v2': 'Bottom'
     }
 
-    base_data_sources = make_dict({
+    base_data_sources = immutable.make_dict({
         'fashion_gallery': dss.fashion.FashionGalleryDataSource(client),
 		'fashion_most_viewed': dss.fashion.FashionMostViewedDataSource(client),
-        'fashion_jcm_on_fashion' : dss.fashion.JCMOnFashion(client)
+        'jcm_on_fashion' : dss.fashion.JCMOnFashion(client)
  
 	})
 
@@ -41,9 +41,13 @@ class FashionStatement(mr.EmailTemplate):
     data_sources['v2'] = data_sources['v1']
 
     priority_list = {
-        'v1': [('fashion_hadley', 1), ('fashion_most_viewed', 6), ('fashion_news', 3), ('fashion_blog', 6), ('fashion_network', 6), ('fashion_gallery', 1)],
-        'v3': [('fashion_hadley', 1), ('fashion_sali', 1), ('fashion_stylewatch', 1), ('fashion_picks', 5), ('fashion_most_viewed', 6), ('fashion_gallery', 1)]
+        'v1': [('jcm_on_fashion', 1), ('fashion_hadley', 1), ('fashion_most_viewed', 6), ('fashion_news', 3), ('fashion_blog', 6), ('fashion_network', 6), ('fashion_gallery', 1)],
+        'v3': [('jcm_on_fashion', 1), ('fashion_hadley', 1), ('fashion_sali', 1), ('fashion_stylewatch', 1), ('fashion_picks', 5), ('fashion_most_viewed', 6), ('fashion_gallery', 1)]
     }
     priority_list['v2'] = priority_list['v1']
 
-    template_names = {'v1': 'fashion-statement-v1', 'v2': 'fashion-statement-v2', 'v3': 'fashion-statement-v3'}
+    template_names = immutable.make_dict({
+        'v1': 'fashion/fashion-statement/v1',
+        'v2': 'fashion/fashion-statement/v2',
+        'v3': 'fashion/fashion-statement/v3',
+        })
