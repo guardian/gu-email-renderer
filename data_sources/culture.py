ce77eeeb598e58c751e62cd9795b32e6078cb578
Robert Rees <robert.rees@guardian.co.uk>
2014-12-22 12:30:49 +0000

    Started to create the Bookmarks email

---
+++ data_sources/culture.py
@@ -0,0 +1,10 @@
+from data_source import DataSource, ItemDataSource, SearchDataSource
+
+class BooksEditorsPicks(ItemDataSource):
+    def __init__(self, client):
+        ItemDataSource.__init__(self, client, content_id='books', show_editors_picks=True)
+
+class BookReviews(ItemDataSource):
+    def __init__(self, client):
+        ItemDataSource.__init__(self, client, content_id='books')
+        self.tags = ['tone/reviews']
--- email_definitions/culture.py
+++ email_definitions/culture.py
@@ -3,6 +3,7 @@ import pysistence as immutable
 import mail_renderer as mr
 
 import data_source as ds
+import data_sources as dss
 
 from guardianapi.apiClient import ApiClient
 
@@ -64,4 +65,29 @@ class SleeveNotes(mr.EmailTemplate):
         'v3': 'sleeve-notes-v3',
         })
 
+class Bookmarks(mr.EmailTemplate):
+    recognized_versions = immutable.make_list('v1')
 
+    ad_tag = 'email-bookmarks'
+    ad_config = immutable.make_dict({
+        'leaderboard_v1': 'Top',
+        'leaderboard_v2': 'Bottom'
+    })
+
+    base_data_sources = immutable.make_dict({
+        'books_picks' : dss.culture.BooksEditorsPicks(client),
+        'book_reviews' : dss.culture.BookReviews(client),
+    })
+
+    data_sources = immutable.make_dict({
+        'v1' : base_data_sources,
+    })
+ 
+    priority_list = immutable.make_dict({
+        'v1': immutable.make_list(
+            ('books_picks', 5),
+            ('book_reviews', 3),)    
+    })
+    template_names = immutable.make_dict({
+        'v1': 'culture/bookmarks/v1',
+    })
