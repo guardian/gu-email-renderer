c332d2b691e0140c092dca6bdd6da5c7ab0d48a6
Robert Rees <robert.rees@guardian.co.uk>
2015-02-17 15:00:08 +0000

    Cache individual urls from CAPI to avoid repeat calls

--- guardianapi/fetchers.py
+++ guardianapi/fetchers.py
@@ -1,14 +1,20 @@
 import logging
 import urllib2
 
+from google.appengine.api import memcache
 
 def best_fetcher():
     return Fetcher()
 
 class Fetcher(object):
     def get(self, url):
+        api_response = memcache.get(url)
+
+        if api_response:
+            return api_response
+
         try:
-            u = urllib2.urlopen(url)
+            u = urllib2.urlopen(url, timeout=12)
         except urllib2.URLError as e:
             if hasattr(e, 'reason'):
                 logging.error('Could not reach server while accessing %s. Reason: %s' % (url, e.reason))
@@ -17,7 +23,11 @@ class Fetcher(object):
             raise e
 
         headers = u.headers.dict
-        return headers, u.read()
+        api_response = (headers, u.read())
+
+        memcache.set(url, api_response, time=2*60)
+
+        return api_response
 
 
 
