31c003ff1c5df2699a847ec22e236a312e30569f
Robert Rees <robert.rees@guardian.co.uk>
2015-02-18 15:32:48 +0000

    Reduces the timeout on an individual fetch and tries to better encapsulate the retry functionality

--- guardianapi/fetchers.py
+++ guardianapi/fetchers.py
@@ -7,6 +7,19 @@ from google.appengine.api import memcache
 def best_fetcher():
     return Fetcher()
 
+def read_url(url, retries=1):
+    try:
+        return urllib2.urlopen(url, timeout=5)
+    except urllib2.URLError as e:
+        if hasattr(e, 'reason'):
+            logging.error('Could not reach server while accessing %s. Reason: %s' % (url, e.reason))
+        elif hasattr(e, 'code'):
+            logging.error('Server could not fulfill request at %s. Error: %s' % (url, e.code))
+        raise e
+    except httplib.HTTPException:
+        logging.warning("HTTP Exception thrown, retries {0} remaining".format(retries))
+        return read_url(url, retries=retries-1)
+
 class Fetcher(object):
     def get(self, url):
         api_response = memcache.get(url)
@@ -14,24 +27,7 @@ class Fetcher(object):
         if api_response:
             return api_response
 
-        def read_url(url):
-            try:
-                u = urllib2.urlopen(url, timeout=12)
-            except urllib2.URLError as e:
-                if hasattr(e, 'reason'):
-                    logging.error('Could not reach server while accessing %s. Reason: %s' % (url, e.reason))
-                elif hasattr(e, 'code'):
-                    logging.error('Server could not fulfill request at %s. Error: %s' % (url, e.code))
-                raise e
-
-            return u
-
-        u = None
-        try:
-            u = read_url(url)    
-        except httplib.HTTPException:
-            logging.warning("HTTP Exception thrown, retrying once")
-            u = read_url(url)
+        u = read_url(url)
 
         headers = u.headers.dict
         api_response = (headers, u.read())
