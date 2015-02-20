0fdad112b334f07c9f4e7555b22578bc70906f50
Robert Rees <robert.rees@guardian.co.uk>
2015-02-20 14:29:11 +0000

    Makes the email definition immutable and adds a title to the scrape page

--- email_definitions/media.py
+++ email_definitions/media.py
@@ -1,24 +1,28 @@
 import mail_renderer as mr
 import data_source as ds
 
+import pysistence as immutable
+
 class MediaBriefing(mr.EmailTemplate):
-    recognized_versions = ['v1']
+    recognized_versions = immutable.make_list('v1')
     cache_bust=True
 
     ad_tag = 'email-media-briefing'
-    ad_config = {
+    ad_config = immutable.make_dict({
         'leaderboard_v1': 'Top',
         'leaderboard_v2': 'Bottom'
-    }
+    })
 
-    data_sources = {}
-    data_sources['v1'] = {
+    data_sources = immutable.make_dict({'v1': {
         'media_stories': ds.MediaDataSource(mr.client),
         'media_monkey': ds.MediaMonkeyDataSource(mr.client),
         'media_briefing': ds.MediaBriefingDataSource(mr.client)
         }
+    })
 
-    priority_list = {}
-    priority_list['v1'] = [('media_stories', 10), ('media_monkey', 1), ('media_briefing', 1)]
+    priority_list = immutable.make_dict({
+    	'v1': immutable.make_list(('media_stories', 10),
+    		('media_monkey', 1), ('media_briefing', 1)),
+    	})
 
-    template_names = {'v1': 'media/media-briefing'}
+    template_names = immutable.make_dict({'v1': 'media/media-briefing'})
