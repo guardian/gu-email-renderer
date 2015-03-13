7667264d274a87361f4eb0c9bb502a4a7a944202
Robert Rees <robert.rees@guardian.co.uk>
2015-03-13 17:49:07 +0000

    Removes money from the email

--- data_sources/us.py
+++ data_sources/us.py
@@ -3,9 +3,3 @@ from data_source import ItemDataSource
 class SportUSDataSource(ItemDataSource):
     def __init__(self, client):
         ItemDataSource.__init__(self, client, 'us/sport', show_editors_picks=True)
-
-class USMoneyDataSource(ItemDataSource):
-    def __init__(self, client):
-        ItemDataSource.__init__(self, client, content_id='us/money', show_editors_picks=True)
-
-
