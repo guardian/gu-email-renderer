54e08165f232491e9ec3d471e37e32f2e2c034b3
Robert Rees <robert.rees@guardian.co.uk>
2015-04-10 15:02:33 +0100

    Removes duplicate filter definition resulting from merge

--- template_filters.py
+++ template_filters.py
@@ -32,23 +32,6 @@ def largest_image(content, image_type='thumbnail'):
 	biggest_image = reduce(widest_image, thumbnails[0]['assets'])
 	return biggest_image
 
-def largest_image(content):
-	thumbnails = [element for element in content['elements'] if element['relation'] == 'main']
-	if not thumbnails:
-		return {}
-
-	def widest_image(current_largest_image, image):
-		if not current_largest_image:
-			return image
-
-		if current_largest_image['typeData']['width'] > image['typeData']['width']:
-			return current_largest_image
-		return image
-
-	largest_thumbnail = reduce(widest_image, thumbnails[0]['assets'])
-
-	return largest_thumbnail
-
 def image_of_width(content, width, image_type='thumbnail'):
 	images = [element for element in content['elements'] if element['relation'] == image_type]
 	assets = images[0].get('assets', [])
