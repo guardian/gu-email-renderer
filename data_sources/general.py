4c0901726167373ab94029ff867b8b691cbccf4f
Robert Rees <robert.rees@guardian.co.uk>
2015-06-05 14:15:36 +0100

    Switch from most read as the default view

--- data_sources/general.py
+++ data_sources/general.py
@@ -3,8 +3,8 @@ from mail_renderer import client
 import data_source
 
 class ItemDataSource(data_source.ItemDataSource):
-	def __init__(self, content_id, production_office=None):
-		data_source.ItemDataSource.__init__(self, client, content_id=content_id, show_most_viewed=True)
+	def __init__(self, content_id, production_office=None, show_most_viewed=False):
+		data_source.ItemDataSource.__init__(self, client, content_id=content_id, show_most_viewed=show_most_viewed)
 
 		if production_office:
 			self.production_office = production_office
