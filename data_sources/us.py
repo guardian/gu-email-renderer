4d64849de0c7427f1bef3c7b1676737b68a6a8b5
Robert Rees <robert.rees@guardian.co.uk>
2015-09-18 16:16:24 +0100

    Add the appropriate changes to the datasources used by the US email to allow the 2015 design to work

--- data_sources/us.py
+++ data_sources/us.py
@@ -3,3 +3,5 @@ from data_source import ItemDataSource
 class SportUSDataSource(ItemDataSource):
     def __init__(self, client):
         ItemDataSource.__init__(self, client, 'us/sport', show_editors_picks=True)
+        self.show_tags = ['keyword']
+        self.show_elements = 'image'
